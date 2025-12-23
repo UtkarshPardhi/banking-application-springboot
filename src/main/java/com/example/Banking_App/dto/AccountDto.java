faa1cb4182ccf5aa13f88d37034ac75649109f3e
UtkarshPardhi <pardhiutkarsh895@gmail.com>
2025-12-24 00:53:06 +0530

    Initial commit - Banking Application (Spring Boot)

---
+++ src/main/java/com/example/Banking_App/dto/AccountDto.java
@@ -0,0 +1,16 @@
+package com.example.Banking_App.dto;
+
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//@AllArgsConstructor
+//@Data
+//public class AccountDto {
+    //private Long id;
+    //private String accountHolderName;
+   // private double balance;
+//}
+
+public record AccountDto(Long id,
+                         String accountHolderName,
+                         double balance) {
+}
---
+++ src/main/java/com/example/Banking_App/mapper/AccountMapper.java
@@ -0,0 +1,29 @@
+package com.example.Banking_App.mapper;
+
+import com.example.Banking_App.dto.AccountDto;
+import com.example.Banking_App.entity.Account;
+
+public class AccountMapper {
+    public  static Account mapToAccount(AccountDto accountDto){
+        Account account = new Account(
+           //accountDto.getId(),
+           //accountDto.getAccountHolderName(),
+                //accountDto.getBalance()
+
+                accountDto.id(),
+                accountDto.accountHolderName(),
+                accountDto.balance()
+        );
+
+        return account;
+    }
+
+    public static AccountDto mapToAccountDto(Account account){
+        AccountDto accountDto = new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalance()
+        );
+        return accountDto;
+    }
+}
